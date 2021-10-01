d97b693f9c2125d68ca4cb7ee524d83694dfa02b
Ye Hui <porcupine920@hotmail.com>
2021-10-01 18:38:29 +0800

    fixed the bug of the inversion of horizontal and vertical direction

--- src/gameoflife/core.cljs
+++ src/gameoflife/core.cljs
@@ -7,11 +7,12 @@
 (def window-width (reagent/atom nil))
 ;;(.setTransform ctx 1, 0, 0, 1, 0.5, 0.5)
 
-(def glider-gun #{[0 24] [1 22] [1 24] [2 12] [2 13] [2 20] [2 21] [2 34] [2 35] [3 11] [3 15] [3 20] [3 21] [3 34] [3 35] [4 0] [4 1] [4 10] [4 16] [4 20] [4 21] [5 0] [5 1] [5 10] [5 14] [5 16] [5 17] [5 22] [5 24] [6 10] [6 16] [6 24] [7 11] [7 15] [8 12] [8 13]})
+(def glider-gun #{[24 0] [22 1] [24 1] [12 2] [13 2] [20 2] [21 2] [34 2] [35 2] [11 3] [15 3] [20 3] [21 3] [34 3] [35 3] [0 4] [1 4] [10 4] [16 4] [20 4] [21 4] [0 5] [1 5] [10 5] [14 5] [16 5] [17 5] [22 5] [24 5] [10 6] [16 6] [24 6] [11 7] [15 7] [12 8] [13 8]})
 
-(def glider-duplicator #{[0 7] [0 12] [0 13] [0 37] [0 38] [1 8] [1 13] [1 37] [1 38] [2 6] [2 7] [2 8] [2 13] [2 15] [2 23] [2 34] [2 35] [2 42] [2 46] [2 47] [3 14] [3 15] [3 23] [3 25] [3 33] [3 34] [3 35] [3 41] [3 45] [3 47] [4 26] [4 27] [4 34] [4 35] [4 42] [4 43] [4 44] [4 45] [4 46] [5 26] [5 27] [5 37] [5 38] [5 43] [5 44] [5 45] [6 26] [6 27] [6 37] [6 38] [7 23] [7 25] [8 23] [11 24] [11 25] [12 24] [12 25] [16 25] [16 26] [16 27] [17 27] [18 26] [21 9] [21 11] [22 7] [22 11] [22 17] [22 18] [22 19] [23 0] [23 1] [23 7] [23 15] [23 17] [23 20] [23 23] [23 24] [24 0] [24 1] [24 6] [24 11] [24 19] [24 20] [24 23] [24 26] [25 7] [25 27] [26 7] [26 11] [26 14] [26 15] [26 16] [26 27] [27 9] [27 11] [27 27] [28 23] [28 26] [28 32] [28 33] [29 23] [29 24] [29 32] [29 34] [30 34] [31 34] [31 35] })
+(def glider-duplicator #{[7 0] [12 0] [13 0] [37 0] [38 0] [8 1] [13 1] [37 1] [38 1] [6 2] [7 2] [8 2] [13 2] [15 2] [23 2] [34 2] [35 2] [42 2] [46 2] [47 2] [14 3] [15 3] [23 3] [25 3] [33 3] [34 3] [35 3] [41 3] [45 3] [47 3] [26 4] [27 4] [34 4] [35 4] [42 4]
+  [43 4] [44 4] [45 4] [46 4] [26 5] [27 5] [37 5] [38 5] [43 5] [44 5] [45 5] [26 6] [27 6] [37 6] [38 6] [23 7] [25 7] [23 8] [24 11] [25 11] [24 12] [25 12] [25 16] [26 16] [27 16] [27 17] [26 18] [9 21] [11 21] [7 22] [11 22] [17 22] [18 22] [19 22] [0 23] [1 23] [7 23] [15 23] [17 23] [20 23] [23 23] [24 23] [0 24] [1 24] [6 24] [11 24] [19 24] [20 24] [23 24] [26 24] [7 25] [27 25] [7 26] [11 26] [14 26] [15 26] [16 26] [27 26] [9 27] [11 27] [27 27] [23 28] [26 28] [32 28] [33 28] [23 29] [24 29] [32 29] [34 29] [34 30] [34 31] [35 31]})
 
-(def initial glider-gun)
+(def initial glider-duplicator)
 
 (def cells (reagent/atom initial))
 
@@ -38,18 +39,18 @@
 
 (defn draw-cell [ctx x y]
   (.beginPath ctx)
-  (.arc ctx (+ y radius) (+ x radius) radius 0 (* 2 js/Math.PI) false)
+  (.arc ctx (+ x radius) (+ y radius) radius 0 (* 2 js/Math.PI) false)
   (set! (.-fillStyle ctx) "lightgreen")
   (.fill ctx)
   (.stroke ctx))
 
 (def scale 20)
 
-(def xoffset 2)
+(def xoffset 0)
 
-(def yoffset 0)
+(def yoffset 2)
 
-(set! yoffset (quot (- (quot (.-innerWidth js/window) scale) (reduce max (map second initial))) 2))
+(set! xoffset (quot (- (quot (.-innerWidth js/window) scale) (reduce max (map first initial))) 2))
 
 (defn transform [loc]
   (* loc scale))
@@ -93,16 +94,16 @@
 
 (defn on-window-resize [ evt ]
   (reset! window-width (.-innerWidth js/window))
-  (set! yoffset (quot (- (quot (.-innerWidth js/window) scale) (reduce max (map second initial))) 2)))
+  (set! xoffset (quot (- (quot (.-innerWidth js/window) scale) (reduce max (map first initial))) 2)))
 
 (defn on-mouse-down [evt]
   (reset! mouse-down? true)
-  (swap! cells #(conj % %2) [(- (quot (.-offsetY evt) scale) xoffset) (- (quot (.-offsetX evt) scale) yoffset)])
+  (swap! cells #(conj % %2) [(- (quot (.-offsetX evt) scale) xoffset) (- (quot (.-offsetY evt) scale) yoffset)])
   (rdom/render [home] (.getElementById js/document "app")))
 
 (defn on-mouse-move [evt]
   (when @mouse-down?
-    (swap! cells #(conj % %2) [(- (quot (.-offsetY evt) scale) xoffset) (- (quot (.-offsetX evt) scale) yoffset)])
+    (swap! cells #(conj % %2) [(- (quot (.-offsetX evt) scale) xoffset) (- (quot (.-offsetY evt) scale) yoffset)])
     (rdom/render [home] (.getElementById js/document "app"))))
 
 (defn on-mouse-up [evt]
